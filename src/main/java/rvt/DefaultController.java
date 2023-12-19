bace1f6bc537595263b2c3acb4663efa4739f884
22DP3LGold <131261185+22DP3LGold@users.noreply.github.com>
2023-12-19 12:18:26 +0000

    mjau mjau

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
@@ -12,4 +13,25 @@ public class DefaultController {
     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
         return "index";
     }
+
+    // @GetMapping(value = "/hello")
+    // String myName = "lauris";
+    // model.addAttribute("name", myName);
+    
+
+
+    @GetMapping(value = "/dziesmina")
+    String dziesmina(){
+        return "dziesmina";
+    }
+    @GetMapping(value = "/about")
+    ModelAndView about() {
+        ModelAndView modelAndView = new ModelAndView("about");
+        
+        return modelAndView;
+    }
+    @GetMapping(value = "/inputins")
+    String inputins(){
+        return "inputins";
+}
 }
---
+++ src/main/java/rvt/blabla.java
@@ -0,0 +1,14 @@
+package rvt;
+
+import java.util.HashMap;
+
+public class blabla {
+    
+    HashMap<String, String> Hobij = new HashMap<String, String>();
+    
+    public void someMethod() {
+        Hobij.put("Name", "Name");
+        Hobij.put("Vecums", "Vecums");
+        Hobij.put("Hobijs", "Hobijs");
+    }
+}
